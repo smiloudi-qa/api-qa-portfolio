a2fec8f872bf06bdd8e10048ed6e77db103c09b7
smiloudi-qa <miloudislaheddine@gmail.com>
2026-08-24 16:05:02 +0200

    fix: update lockfile and config for CI pipeline

--- playwright.config.js
+++ playwright.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  workers: 1, // Exécution séquentielle pour éviter les conflits d'API
   reporter: [['html', { open: 'never' }], ['list']],
   use: {
-    baseURL: 'https://restful-booker.herokuapp.com/',
     extraHTTPHeaders: {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
